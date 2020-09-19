1df96979765f5634f4bdb2d6bc2b51bf36a500d1
ogbajijames20 <ogbajijames25@gmail.com>
2020-09-19 10:19:36 +0100

    I created loginpage and modified contactpage

--- login.php
+++ login.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login Page | ConnectMe</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div id="container">
-        
+        <section>
+            
+        </section>
     </div>
 </body>
 </html>
