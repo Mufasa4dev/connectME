7ba3014386aae58372e66500faf45062601b873c
ogbajijames20 <ogbajijames25@gmail.com>
2020-09-19 10:05:06 +0100

    I created loginpage and made changes on contactpage

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div id="container">
+        <section>
+            <div>
+                
+            </div>
+        </section>
+    </div>
+</body>
+</html>
